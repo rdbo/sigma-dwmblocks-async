3b11c3e6b9f989c10feb2c78f552c77302b77ae1
rdbo <rdbodev@gmail.com>
2022-10-14 08:35:19 -0300

    added battery scripts

--- config.h
+++ config.h
@@ -3,12 +3,13 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sbtime",           1, 17),
-        BLOCK("sbnwif enp1s0",    5, 18),
-        BLOCK("sbnwif -w wlp2s0", 5, 19),
-        BLOCK("sbmemory",         5, 20),
-        BLOCK("sbnetwork",        1, 21),
-        BLOCK("sbaudio sink",     1, 22),
-        BLOCK("sbaudio source",   1, 23),
-        BLOCK("sbpower",          0, 24),
+	BLOCK("sbtime",           1,  17),
+        BLOCK("sbnwif enp1s0",    5,  18),
+        BLOCK("sbnwif -w wlp2s0", 5,  19),
+        BLOCK("sbmemory",         5,  20),
+        BLOCK("sbnetwork",        1,  21),
+        BLOCK("sbbattery BAT1",   10, 22),
+        BLOCK("sbaudio sink",     1,  23),
+        BLOCK("sbaudio source",   1,  24),
+        BLOCK("sbpower",          0,  25),
 };
