6e51e11c9d3a45da2fc689bdbc1f89c83dbb7a02
user <user@localhost>
2022-10-14 08:24:08 -0300

    updated config

--- config.h
+++ config.h
@@ -1,16 +1,14 @@
 #define CMDLENGTH 45
-#define DELIMITER "  "
+#define DELIMITER " | "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("sbtime",           1, 17),
+        BLOCK("sbnwif enp1s0",    5, 18),
+        BLOCK("sbnwif -w wlp2s0", 5, 19),
+        BLOCK("sbmemory",         5, 20),
+        BLOCK("sbnetwork",        1, 21),
+        BLOCK("sbaudio sink",     1, 22),
+        BLOCK("sbaudio source",   1, 23),
+        BLOCK("sbpower",          0, 24),
 };
