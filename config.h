0f43f1d82405ce30d596bf9d7afddd41abf69c1d
rdbo <rdbodev@gmail.com>
2022-10-16 09:57:28 -0300

    added virtual keyboard module

--- config.h
+++ config.h
@@ -5,11 +5,12 @@
 const Block blocks[] = {
         BLOCK("sbnetwork",        1,  17),
 	BLOCK("sbtime",           1,  18),
-        BLOCK("sbnwif eth0",    5,  19),
-        BLOCK("sbnwif -w wlan0", 5,  20),
+        BLOCK("sbnwif eth0",      5,  19),
+        BLOCK("sbnwif -w wlan0",  5,  20),
         BLOCK("sbmemory",         5,  21),
         BLOCK("sbbattery BAT1",   10, 22),
         BLOCK("sbaudio sink",     1,  23),
         BLOCK("sbaudio source",   1,  24),
-        BLOCK("sbpower",          0,  25),
+        BLOCK("sbvirtkbd",        0,  25),
+        BLOCK("sbpower",          0,  26),
 };
